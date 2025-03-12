11e3e4d9bd4c76bdb58d9d7b81a1fbdf30320bb7
Vlados5808 <vvladeslav70@gmail.com>
2025-03-12 15:53:06 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -3,22 +3,22 @@ const app = express();
 const port = 3000;
 
 app.get('/hello', (req, res) => {
-    res.send('Привіт, світ!');
+    res.send(`Привіт, світ!`);
 });
 
 app.get('/json', (req, res) => {
-    res.json({ message: 'Це JSON-відповідь' });
+    res.json({ message: `Це JSON-відповідь` });
 });
 
 app.get('/user/:id', (req, res) => {
-    res.send('Користувач з ID: ${req.params.id}');
+    res.send(`Користувач з ID: ${req.params.id}`);
 });
 
 app.get('/search', (req, res) => {
     const query = req.query.q;
-    res.send('Пошук за запитом: ${query}');
+    res.send(`Пошук за запитом: ${query}`);
 });
 
 app.listen(port, () => {
-    console.log('Сервер працює на http://localhost:${port}');
+    console.log(`Сервер працює на http://localhost:${port}`);
 });
